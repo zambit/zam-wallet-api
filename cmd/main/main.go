d66ac3da1425b3102e102bbddf83873767280d53
danields <danilzam@zam.me>
2018-09-14 15:18:12 +0300

    feat(ci): add embedded build info into executable about version

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"git.zam.io/wallet-backend/wallet-api/cmd/listener"
 	"git.zam.io/wallet-backend/wallet-api/cmd/root"
 	"git.zam.io/wallet-backend/wallet-api/cmd/server"
@@ -10,6 +11,14 @@ import (
 	"github.com/spf13/viper"
 )
 
+var (
+	commitSHA   = "undefined"
+	commitRef   = "undefined"
+	commitRep   = "undefined"
+	commitEnv   = "undefined"
+	commitPipID = "undefined"
+)
+
 // main executes specified command using cobra, on error will panic for nice stack print and non-zero exit code
 func main() {
 	var cfg config.RootScheme
@@ -17,6 +26,17 @@ func main() {
 
 	config.Init(v)
 	rootCmd := root.Create(v, &cfg)
+	rootCmd.Version = fmt.Sprintf("%s-%s", commitRef, commitSHA)
+	rootCmd.Long = fmt.Sprintf(
+		`
+Repository: %s
+Built for env: %s
+Built in pipeline: %s
+		`,
+		commitRep,
+		commitEnv,
+		commitPipID,
+	)
 	serverCmd := server.Create(v, &cfg)
 	watcherCmd := watcher.Create(v, &cfg)
 	listenerCmd := listener.Create(v, &cfg)
